2c91bc55a78f30a38f82f1c35305c94245b03f43
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-22 18:27:17 -0400

    beg. clusters!

--- March22.R
+++ March22.R
@@ -1,1 +1,7 @@
-### Hello 
+### Hello its the NHL!
+
+
+###----------------------------------------------------------------###
+
+TopFour <- read.csv("Skaters_March22_v2.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+
