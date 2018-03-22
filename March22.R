019831c8b68fc4e7034b01c2fc6d5f91e4011351
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-22 18:49:12 -0400

    dd

--- March22.R
+++ March22.R
@@ -1,7 +1,38 @@
 ### Hello its the NHL!
 
+library(foreign)
+library(car)
+library(effects)
+library(doBy)
+library(ggfortify)
+library(cluster)
+library(multcomp)
+library(fpc)
+library(gridExtra)
+library(tibble)
+
 
 ###----------------------------------------------------------------###
 
 TopFour <- read.csv("Skaters_March22_v2.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+
+###----------------------------------------------------------------###
+
+TopFour.Selected <- TopFour[, c("G", "CorsiFor", "xGF")] 
+set.seed(500)
+
+TopFourClusters <- clusGap(TopFour.Selected, FUN = kmeans, K.max = 10, B = 3)
+findClusters
+plot(findClusters)
+
+dat.chicocustomer.cluster <- kmeans(dat.chicocustomer.selected, centers = 3)
+dat.chicocustomer.cluster
+names(dat.chicocustomer.cluster)
+dat.chicocustomer.cluster$centers
+
+# fit series of one-way ANOVAs
+lapply(names(dat.chicocustomer.selected), function(x) Anova(lm(paste(x, " ~ cluster"), dat.chicocustomer.selected.clust)))
+
+autoplot(dat.chicocustomer.cluster, data = dat.chicocustomer, frame = TRUE)
+
 
