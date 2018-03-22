2df006082d747711b962ed815f2fa6aee58ce817
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-22 18:27:17 -0400

    hello2

--- March22.R
+++ March22.R
@@ -1,1 +1,7 @@
 ### Hello 
+
+
+###----------------------------------------------------------------###
+
+TopFour <- read.csv("Skaters_March22_v2.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+
