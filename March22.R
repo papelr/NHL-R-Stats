1f0593e5e9680793213ba6fb956713034075ae4d
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-23 16:24:13 -0400

    commit cluster

---
+++ March22.R
@@ -0,0 +1,59 @@
+### Hello its the NHL!
+
+install.packages("factoextra")
+
+library(foreign)
+library(car)
+library(effects)
+library(doBy)
+library(ggfortify)
+library(cluster)
+library(multcomp)
+library(fpc)
+library(gridExtra)
+library(tibble)
+library(ggplot2)
+library(ggrepel)
+library(dplyr)
+library(tidytext)
+library(factoextra)
+
+
+###----------------------------------------------------------------###
+
+TopFour <- read.csv("Skaters_March22_v2.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+
+###----------------------------------------------------------------###
+
+LineFit <- lm(G ~ CorsiFor, data = TopFour)
+qqPlot(LineFit, main = "QQ Plot")
+leveragePlots(LineFit)
+spreadLevelPlot(LineFit)
+LineFit
+
+
+TopFour.Selected <- TopFour[, c( "G", "xGF", "CorsiFor")] 
+set.seed(500)
+
+TopFour <- na.omit(TopFour.Selected)
+
+TopFourCluster <- clusGap(TopFour.Selected, FUN = kmeans, K.max = 10, B = 3)
+findClusters
+plot(findClusters)
+
+TopFourCluster <- kmeans(TopFour.Selected, centers = 3)
+TopFourCluster
+names(TopFourCluster)
+TopFourCluster$centers
+
+
+TopFour.Selected %>%
+  as_tibble() %>%
+  mutate(cluster = TopFourCluster$cluster,
+         Player = row.names(TopFour)) %>%
+  ggplot(aes(G, xGF, color = factor(cluster), label = Player)) +
+  geom_text() +
+  geom_label((aes(fill = Player)))
+
+ClusterPlot4 <- fviz_cluster(TopFourCluster, geom = "point",  data = TopFour.Selected) + ggtitle("3 Cluster Solution")
+ClusterPlot4
