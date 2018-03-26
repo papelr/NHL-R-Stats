44807fa2760fd7354be2ac1f9b038f8c5bb88c03
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-26 19:17:55 -0400

    gettin' there (not)

--- NSH_vs_BOS_March_26.R
+++ NSH_vs_BOS_March_26.R
@@ -1,1 +1,40 @@
-### ------ NSH vs BOS Scatterplot ------ @Ashley's Dad #####
+### --------- NSH vs BOS Scatterplot ------ @Ashley's Dad #####
+
+library(foreign)
+library(car)
+library(effects)
+library(doBy)
+library(ggfortify)
+library(cluster)
+library(multcomp)
+library(fpc)
+library(gridExtra)
+library(tibble)
+library(ggplot2)
+library(ggrepel)
+library(dplyr)
+library(tidytext)
+library(factoextra)
+
+BosNsh <- read.csv("March26_NSH_BOS.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+
+#### ------------------------------------------------------####
+
+LineFit <- lm(Corsi ~ RelCF, data = BosNsh)
+qqPlot(LineFit, main = "QQ Plot")
+leveragePlots(LineFit)
+spreadLevelPlot(LineFit)
+LineFit
+
+PosColors <- c(D = "darkgoldenrod" , L = "blue2" , C = "darkgreen" , R = "firebrick3") 
+
+
+
+
+
+
+
+
+
+
+
