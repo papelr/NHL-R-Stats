45d9421111f24b40f538577a207c8ae378233e27
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-26 20:20:11 -0400

    Ugh

--- NSH_vs_BOS_March_26.R
+++ NSH_vs_BOS_March_26.R
@@ -26,11 +26,25 @@ leveragePlots(LineFit)
 spreadLevelPlot(LineFit)
 LineFit
 
-PosColors <- c(D = "darkgoldenrod" , L = "blue2" , C = "darkgreen" , R = "firebrick3") 
 
+PlayerPlot <- ggplot(BosNsh, aes(x = Corsi, y = RelCF, color = Position)) + 
+  geom_point(size = 2, shape = 9, show.legend = TRUE) +
+  coord_cartesian(xlim = c(40, 60), ylim = c(-10, 10)) + 
+  geom_smooth(method = "lm", se = FALSE, size = 0.5, color = "black")
 
+PlayerPlot <- PlayerPlot +
+  ggtitle(expression(atop("Corsi vs. Relative Corsi", atop(italic("Boston vs. Nashville (All Positions)"))))) +
+  labs(x = "Corsi", y = "Relative Corsi") +
+  theme(axis.title.y = element_text(size = 12, face = "bold", color = "black")) +
+  theme(axis.title.x = element_text(size = 12, face = "bold", color = "black")) 
 
-
+PlayerPlot <- PlayerPlot +
+  geom_text_repel(aes(label = Player), 
+                   show.legend = FALSE,
+                   min.segment.length = 3,
+                   force = 4)
+                   
+PlayerPlot
 
 
 
