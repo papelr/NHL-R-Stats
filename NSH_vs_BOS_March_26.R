28426c8d8b2bc5792a389f459981ee5fdccc6f16
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-03-27 21:26:41 -0400

    ugh @ geom_label_repel

--- NSH_vs_BOS_March_26.R
+++ NSH_vs_BOS_March_26.R
@@ -16,21 +16,28 @@ library(dplyr)
 library(tidytext)
 library(factoextra)
 
-BosNsh <- read.csv("March26_NSH_BOS.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
+#### ------------------------------------------------------####
+
+BOS_NSH <- read.csv("March26_NSH_BOS.csv", header = TRUE , sep = "," , stringsAsFactors = FALSE)
 
 #### ------------------------------------------------------####
 
-LineFit <- lm(Corsi ~ RelCF, data = BosNsh)
+LineFit <- lm(Corsi ~ RelCF, data = BOS_NSH)
 qqPlot(LineFit, main = "QQ Plot")
 leveragePlots(LineFit)
 spreadLevelPlot(LineFit)
 LineFit
 
 
-PlayerPlot <- ggplot(BosNsh, aes(x = Corsi, y = RelCF, color = Position)) + 
-  geom_point(size = 2, shape = 15, show.legend = TRUE) +
+PlayerPlot <- ggplot(BOS_NSH, aes(x = Corsi, y = RelCF, color = Team)) + 
+  geom_point(size = 2, 
+             shape = 15, 
+             show.legend = TRUE) +
   coord_cartesian(xlim = c(40, 60), ylim = c(-10, 10)) + 
-  geom_smooth(method = "lm", se = FALSE, size = 0.5, color = "black")
+  geom_smooth(method = "lm", 
+              se = FALSE, 
+              size = 0.3, 
+              color = "black")
 
 PlayerPlot <- PlayerPlot +
   ggtitle(expression(atop("Corsi vs. Relative Corsi", atop(italic("Boston vs. Nashville (All Positions)"))))) +
@@ -39,10 +46,11 @@ PlayerPlot <- PlayerPlot +
   theme(axis.title.x = element_text(size = 12, face = "bold", color = "black")) 
 
 PlayerPlot <- PlayerPlot +
-  geom_text_repel(aes(label = Player, color = Team), 
+  geom_label_repel(aes(label = Player), 
                    show.legend = FALSE,
                    min.segment.length = 3,
-                   force = 4)
+                   force = 4
+                   )
                    
 PlayerPlot
 
