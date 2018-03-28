9339f10e15526e8095ea84a691b51b0acbad2efc
papelr <robertpapel@gmail.comgit config --global user.name papelrgit config --global user.email robertpapel@gmail.com>
2018-03-28 18:09:32 -0400

    w/ ggsave

--- NSH_vs_BOS_March_28.R
+++ NSH_vs_BOS_March_28.R
@@ -32,13 +32,11 @@ spreadLevelPlot(LineFit)
 summary(LineFit)
 LineFit
 
-
-
-PlayerPlot <- ggplot(BOS_NSH, aes(x = Corsi, y = RelCF, color = Team)) + 
+PlayerPlot <- ggplot(BOS_NSH, aes(x = GF, y = GA, color = Team)) + 
   geom_point(size = 2.75, 
              shape = 15, 
              show.legend = TRUE) +
-  coord_cartesian(xlim = c(40, 60), ylim = c(-10, 10)) + 
+  coord_cartesian(xlim = c(0, 70), ylim = c(0, 70)) + 
   geom_smooth(method = "lm", 
               se = FALSE, 
               size = 0.3, 
@@ -46,8 +44,8 @@ PlayerPlot <- ggplot(BOS_NSH, aes(x = Corsi, y = RelCF, color = Team)) +
               weight = 0.5)
 
 PlayerPlot <- PlayerPlot +
-  ggtitle(expression(atop("Corsi vs. Relative Corsi", atop(italic("Boston vs. Nashville (All Positions)"))))) +
-  labs(x = "Corsi", y = "Relative Corsi") +
+  ggtitle(expression(atop("Goals For vs. Goals Against", atop(italic("Boston vs. Nashville (All Positions)"))))) +
+  labs(x = "Goals For (GF)", y = "Goals Against (GA)") +
   theme(axis.title.y = element_text(size = 12, face = "bold", color = "black")) +
   theme(axis.title.x = element_text(size = 12, face = "bold", color = "black")) 
 
@@ -58,10 +56,21 @@ PlayerPlot <- PlayerPlot +
                    force = 4,
                    color = "black", 
                    size = 3.55) +
-  theme_bw()
+  theme_bw() +
+  theme(legend.background = element_rect(fill = "gray95")) +
+  theme(legend.box.margin = margin())
                    
 PlayerPlot
 
+PlayerPlot <- ggsave(NHLSkaterPlots, 
+                     plot = PlayerPlot(), 
+                     device = "png", 
+                     path = Users/robertpapel/Documents/NHL R Stuff/NHLSkaterPlots, 
+                     scale = 1, 
+                     width = NA, 
+                     height = NA, 
+                     units = "in", 
+                     limitsize = TRUE)
 
 
 
