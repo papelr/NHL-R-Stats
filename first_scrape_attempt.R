6fedd5bffdc46a140293c55747951bbcd5fe1467
papelr <robertpapel@gmail.com>
2018-04-05 11:26:57 -0400

    Team colors inserted, used reorder() argument

--- first_scrape_attempt.R
+++ first_scrape_attempt.R
@@ -5,6 +5,7 @@
 
 library(tidyverse)
 library(rvest)
+library(dplyr)
 
 url <- "http://www.espn.com/nhl/standings/_/group/league"
 webpage <- read_html(url)
@@ -34,9 +35,46 @@ str(team_points_df)
 
 #'#### **Visualization** #####
 
-ggplot(team_points_df, aes(x = Team, y = Points, color = Team)) +
+teamColor <- c("#FC4C02", 
+               "#8C2633", 
+               "#FFB81C", 
+               "#041E42",
+               "#C8102E", 
+               "#CC0000", 
+               "#C8102E", 
+               "#6F263D", 
+               "#041E42", 
+               "#006341", 
+               "#C8102E", 
+               "#041E42", 
+               "#B9975B", 
+               "#000000", 
+               "#154734", 
+               "#A6192E", 
+               "#FFB81C", 
+               "#C8102E", 
+               "#00468B", 
+               "#0038A8", 
+               "#C8102E", 
+               "#FA4616", 
+               "#CFC493", 
+               "#003087", 
+               "#006272", 
+               "#00205B", 
+               "#00205B", 
+               "#00843D", 
+               "#B9975B", 
+               "#041E42", 
+               "#53565A")
+
+# Team colors, in order by alphabetical order, from:
+# https://teamcolorcodes.com/winnipeg-jets-color-codes/
+
+ggplot(team_points_df, 
+       aes(x = reorder(Team, Points), y = Points, fill = Team)) +
   geom_col(show.legend = FALSE) +
-  coord_flip() 
+  coord_flip() + 
+  scale_fill_manual(values = teamColor)
 
 
 
