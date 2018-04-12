c81fa67e758c6001cf06a106e9cbf86c71c638f5
papelr <robertpapel@gmail.com>
2018-04-12 11:30:22 -0400

    grid arrange. getting there

--- Visualization Frames/histogram_frame.R
+++ Visualization Frames/histogram_frame.R
@@ -1,7 +1,7 @@
 #' ------------------------
 #' Title: **NSH vs COL**
-#' Subtitle: Pre-First Round Matchup
-#' Date: April 9, 2018
+#' Subtitle: Histogrammmmmmmmmmmmmm of G, xGF, iCF
+#' Date: April 12, 2018
 #' By: Robert Papel
 #' ------------------------
 
@@ -9,6 +9,8 @@
 
 library(tidyverse)
 library(rmarkdown)
+library(ggrepel)
+library(gridExtra)
 
 firstRound <- read.csv("preds_avs.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
@@ -19,11 +21,10 @@ teamColor <- c("#6F263D", # Colorado
                "#FFB81C") # Nashville
 
 
-
-ggplot(firstRound) +
-  geom_histogram(aes(x = G, fill = Team), 
+plot1 <- ggplot(firstRound) +
+  geom_histogram(aes(x = ixGF, fill = Team), 
                  bins = 10,
-                 binwidth = 3,
+                 binwidth = 1,
                  show.legend = T,
                  color = "black") +
   scale_fill_manual(values = teamColor) +
@@ -48,10 +49,47 @@ ggplot(firstRound) +
     axis.title.y = element_text(size = 10, face = "bold", color = "black"),
     axis.title.x = element_text(size = 10, face = "bold", color = "black")
     ) +
-  facet_wrap(~Position, nrow = 2)
+  facet_wrap(~Team, ncol = 2)
     
 
+plot2 <- ggplot(firstRound) +
+  geom_histogram(aes(x = IndCorsi, fill = Team), 
+                 bins = 10,
+                 binwidth = 1,
+                 show.legend = T,
+                 color = "black") +
+  scale_fill_manual(values = teamColor) +
+  coord_flip() +
+  labs(
+    title = "Goal Count",
+    subtitle = "Goal Count, Both Teams",
+    x = "Goals", 
+    y = "Number of Players",
+    caption = "Stats from Corsia, plot by R. Papel, April 12, 2018"
+  ) +
+  theme(
+    plot.title = element_text(face = "bold", size = 12),
+    plot.subtitle = element_text(face = "italic"),
+    axis.ticks = element_line(colour = "grey70", size = 0.2),
+    panel.grid.major = element_line(colour = "grey70", size = 0.2),
+    panel.grid.minor = element_blank(),
+    legend.background = element_rect(fill = "grey90", 
+                                     size = 3, colour = "white"),
+    legend.text = element_text(face = "bold"),
+    legend.title = element_text(face = "bold"),
+    axis.title.y = element_text(size = 10, face = "bold", color = "black"),
+    axis.title.x = element_text(size = 10, face = "bold", color = "black")
+  ) +
+  facet_wrap(~Team, ncol = 2)
   
+#' **Putting two plots into one graph (or 4?)**
+
+grid.arrange(plot1, plot2, ncol = 2)
+
+
+
+
+
 
 
 
