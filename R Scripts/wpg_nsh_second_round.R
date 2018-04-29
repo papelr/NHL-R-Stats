100576035c96a24551d46ef4646dab8f7565c48f
Robert Papel <papelr@Roberts-MacBook-Air-2.local>
2018-04-29 13:27:10 -0400

    text cleaning experiment

---
+++ R Scripts/wpg_nsh_second_round.R
@@ -0,0 +1,15 @@
+#' ---------------
+#' Title: Winnipeg vs. Nashville
+#' Subtitle: Second Round
+#' Author: Robert Papel
+#' Date: April 29, 2018
+#' ---------------
+
+#'###### ---------------**Libraries**-------------------- ######
+
+library(tidyverse)
+library()
+
+secondRound <- read.csv("wpg_nsh_april.csv")
+
+#'###### ---------------**Cleaning**-------------------- ######
