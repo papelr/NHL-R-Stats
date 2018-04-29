e6a98ef2271fbe2be335119d571c85768ed090e7
papelr <robertpapel@gmail.com>
2018-04-29 16:06:51 -0400

    FIGURED OUT HOW TO PUT A SPACE

--- R Scripts/wpg_nsh_second_round.R
+++ R Scripts/wpg_nsh_second_round.R
@@ -5,13 +5,12 @@
 #' Date: April 29, 2018
 #' ---------------
 
-#'###### ---------------**Libraries**-------------------- ######
+#'###### ---------------**Libraries & Read-in**-------------------- ######
 
 library(tidyverse)
 library(tm)
 library(stringr)
 library(lettercase)
-
 
 secondRound <- read.csv("wpg_nsh_april.csv")
 str(secondRound)
@@ -19,15 +18,17 @@ head(secondRound)
 
 #'###### ---------------**Cleaning**-------------------- ######
 
+secondRound$Player <- gsub("[[:punct:]]", " ", secondRound$Player, fixed = F)
+secondRound$Player <- tolower(secondRound$Player)
+secondRound$Player <- str_cap_words(secondRound$Player)
+secondRound$Player <- gsub("([a-z])([A-Z])", "\\1 \\2", secondRound$Player)
+
+
 
 secondRound$Player <- gsub("[[:punct:]]", " ", secondRound, fixed = F)
 secondRound <- tolower(secondRound)
 
-secondRound %>% 
-  select("Player") 
 
-secondRound$Player <- as.String(secondRound$Player)
-secondRound
   
   
 
